33c0d0f5445e51fb6a71c2a909b8376a05d102b6
Mufaro <119020374+Mufaro08@users.noreply.github.com>
2025-04-11 19:36:18 +0200

    Added the Dashboard

--- Controllers/Dashboard.cs
+++ Controllers/Dashboard.cs
@@ -1,114 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿ using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using ZambaFarm.Data;
+using System.Threading.Tasks;
 using ZambaFarm.Models;
 
-[Route("Dashboard")]
-public class DashboardController : Controller
+namespace ZambaFarm.Controllers
 {
-    private readonly FarmContext _context;
-
-    public DashboardController(FarmContext context)
-    {
-        _context = context;
-    }
-
-    // Serve the Dashboard Index View
-    [HttpGet]
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    // API for Animal Counts
-    [HttpGet("api/animalCounts")]
-    public IActionResult GetAnimalCounts()
-    {
-        var animalCounts = new
-        {
-            Goats = _context.Goats.Count(),
-            Pigs = _context.Pigs.Count(),
-            Cows = _context.Cattles.Count(),
-            Rabbits = _context.Rabbits.Count(),
-            Turkeys = _context.Turkeys.Count()
-        };
-
-        return Ok(animalCounts);
-    }
-
-    [HttpGet("api/monthlyAnimalCounts")]
-    public IActionResult GetMonthlyAnimalCounts()
-    {
-        var monthlyCounts = _context.Goats
-    .GroupBy(a => new { Year = a.DateAdded.Year, Month = a.DateAdded.Month })
-    .Select(g => new
-    {
-        Species = "Goats",
-        Year = g.Key.Year,
-        Month = g.Key.Month,
-        Count = g.Count()
-    })
-    .Union(
-        _context.Pigs.GroupBy(a => new { Year = a.DateAdded.Year, Month = a.DateAdded.Month })
-        .Select(g => new
-        {
-            Species = "Pigs",
-            Year = g.Key.Year,
-            Month = g.Key.Month,
-            Count = g.Count()
-        })
-    )
-    .Union(
-        _context.Cattles.GroupBy(a => new { Year = a.DateAdded.Year, Month = a.DateAdded.Month })
-        .Select(g => new
-        {
-            Species = "Cows",
-            Year = g.Key.Year,
-            Month = g.Key.Month,
-            Count = g.Count()
-        })
-    )
-    .Union(
-        _context.Rabbits.GroupBy(a => new { Year = a.DateAdded.Year, Month = a.DateAdded.Month })
-        .Select(g => new
-        {
-            Species = "Rabbits",
-            Year = g.Key.Year,
-            Month = g.Key.Month,
-            Count = g.Count()
-        })
-    )
-    .Union(
-        _context.Turkeys.GroupBy(a => new { Year = a.DateAdded.Year, Month = a.DateAdded.Month })
-        .Select(g => new
-        {
-            Species = "Turkeys",
-            Year = g.Key.Year,
-            Month = g.Key.Month,
-            Count = g.Count()
-        })
-    )
-    .ToList();
-
-
-        return Ok(monthlyCounts);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*public class DashboardController : Controller
+       public class DashboardController : Controller
     {
         private readonly FarmContext _context;
 
@@ -141,5 +39,6 @@ public class DashboardController : Controller
 
             return View();
         }
-    }*/
+    }
+
 }
---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ZambaFarm.Data;
+using System.Data;
+
+namespace ZambaFarm.Models
+{
+    public class DashboardViewModel
+    {
+        public object TotalAnimals { get; set; }
+        public object AnimalGrowth { get; set; }
+    }
+
+    public class AnimalCount
+    {
+        public int Rabbits { get; set; }
+        public int Pigs { get; set; }
+        public int Cattles { get; set; }
+        public int Goats { get; set; }
+        public int Turkeys { get; set; }
+    }
+
+    public class AnimalGrowth
+    {
+        public List<SpeciesGrowth> Rabbits { get; set; }
+        public List<SpeciesGrowth> Pigs { get; set; }
+        public List<SpeciesGrowth> Cattles { get; set; }
+        public List<SpeciesGrowth> Goats { get; set; }
+        public List<SpeciesGrowth> Turkeys { get; set; }
+    }
+
+    public class SpeciesGrowth
+    {
+        public int Month { get; set; }
+        public int Count { get; set; }
+    }
+}
+
